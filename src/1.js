4271438b07e48f68bcdbc4b5eb499e56a21da350
azenkurt <azenkurit@gmail.com>
2019-10-08 17:44:30 +0300

    ref: math0
    no brackets

--- src/1.js
+++ src/1.js
@@ -1,37 +1,41 @@
 function expressionCalculator(expr) {
     let q = expr.replace(/\s/g, '');
-    let w = q.split(/(\+||\-||\*||\\)/);
-    let finArr = [];
-    for ( let i=0; i<w.length; i++) {
-        switch ( w[i] ) {
-            case '' : {
-                break;
-            };
-            case '*' : {
-                finArr.push ( w[i-1] * w[i+1] );
-                i += 2;
-                break;
-            };
-            case '\\' : {
-                finArr.push ( w[i-1] / w[i+1] );
-                i += 2;
-                break;
-            };
-            case '+' : {
-                finArr.push ( w[ i-1 ]/1 + w[ i+1]/1 );
-                i += 2;
-                break;
-            };
-            case '-' : {
-                finArr.push ( w[ i-1]/1 - w[i+1]/1 );
-                i += 2;
-                break;
-            };
-            default : {
-                finArr.push ( w[i] );
-            };
-        };
+    let numbers = q.split(new RegExp("[\/\*\+\-]")); 
+    let symbols = [];
+    for ( let i = 0; i<q.length; i++ ) {
+        if ( q[i] === "+" || q[i] === "-" || q[i] === "*" || q[i] === "/" ) symbols.push( q[i] );
+    }    
+    while ( symbols.indexOf("*") + symbols.indexOf("/") > -2 ) {        
+        for ( let i = 0; i<symbols.length ; i++) {    
+            if ( symbols[i] === "*" || symbols[i] === "/") {       
+                numbers[i] = symbols[i] === "*" ? numbers[ i ] * numbers[ i+1 ] : numbers[ i ] / numbers[ i+1 ];
+                    for ( let j = i + 1 ; j < numbers.length ; j++ ) {
+                        numbers[j] = numbers[ j+1 ];                                  
+                    };
+                    for ( let j = i; j < symbols.length ; j++ ) {
+                        symbols[j] = symbols[ j+1 ];
+                    };    
+                    numbers.length -= 1;
+                    symbols.length -= 1;             
+                }           
+            }
     };
-    console.log ( finArr );
+    while ( symbols.indexOf("+") + symbols.indexOf("-") > -2 ) {  
+        for ( let i = 0; i<symbols.length ; i++) {    
+            if ( symbols[i] === "+" || symbols[i] === "-") {       
+                numbers[i] = symbols[i] === "+" ? numbers[ i ]/1 + numbers[ i+1 ]/1 : numbers[ i ]/1 - numbers[ i+1 ]/1;
+                    for ( let j = i + 1 ; j < numbers.length ; j++ ) {
+                        numbers[j] = numbers[ j+1 ];                                  
+                    };
+                    for ( let j = i; j < symbols.length ; j++ ) {
+                        symbols[j] = symbols[ j+1 ];
+                    };    
+                numbers.length -= 1;
+                symbols.length -= 1;             
+            }           
+        };
+    };                      
+    console.log ( numbers[0] );
+    console.log ( symbols );        
 };
-expressionCalculator('2 + 2-5*61');
+expressionCalculator('84 + 62 / 33 * 10 + 15');
--- src/index.js
+++ src/index.js
@@ -4,40 +4,47 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    let arr = expr.split(RegExp(/\+||\-||\*||\\/));
+    let q = expr.replace(/\s/g, '');
+    let numbers = q.split(new RegExp("[\/\*\+\-]")); 
+    let symbols = [];
+    console.log ( expr );
+    console.log ( numbers.join('_') );
     
-    while ( arr.length >1 ) {
-        let fArr = [];
-        for ( let i = 0; i< arr.length; i ++ ) {
-            switch ( arr [i] ) {
-                case "*" : {
-                    fArr.push ( arr[ i-1 ]/1 * arr[ i+1 ]/1 );
-                    i += 2;
-                    break;
-                };
-                case "\\" : {
-                    fArr.push ( arr[ i-1 ]/1 / (arr[ i+1 ]/1) );
-                    i += 2;
-                    break;
-                };
-                case "+" : {
-                    fArr.push ( arr[ i-1 ]/1 + arr[ i+1 ]/1 );
-                    i += 2;
-                    break;
-                };
-                case "-" : {
-                    fArr.push ( arr[ i-1 ]/1 - arr[ i+1 ]/1 );
-                    i += 2;
-                    break;
-                };
-                default : {
-                    fArr.push ( arr[i] )
-                };
-            };        
+    for ( let i = 0; i<q.length; i++ ) {
+        if ( q[i] === "+" || q[i] === "-" || q[i] === "*" || q[i] === "/" ) symbols.push( q[i] );
+    }    
+    console.log ( symbols.join('_') );
+    while ( symbols.indexOf("*") + symbols.indexOf("/") > -2 ) {        
+        for ( let i = 0; i<symbols.length ; i++) {    
+            if ( symbols[i] === "*" || symbols[i] === "/") {       
+                numbers[i] = symbols[i] === "*" ? numbers[ i ] * numbers[ i+1 ] : numbers[ i ] / numbers[ i+1 ];
+                    for ( let j = i + 1 ; j < numbers.length ; j++ ) {
+                        numbers[j] = numbers[ j+1 ];                                  
+                    };
+                    for ( let j = i; j < symbols.length ; j++ ) {
+                        symbols[j] = symbols[ j+1 ];
+                    };    
+                    numbers.length -= 1;
+                    symbols.length -= 1;             
+                }           
+            }
+    };
+    while ( symbols.indexOf("+") + symbols.indexOf("-") > -2 ) {  
+        for ( let i = 0; i<symbols.length ; i++) {    
+            if ( symbols[i] === "+" || symbols[i] === "-") {       
+                numbers[i] = symbols[i] === "+" ? numbers[ i ]/1 + numbers[ i+1 ]/1 : numbers[ i ]/1 - numbers[ i+1 ]/1;
+                    for ( let j = i + 1 ; j < numbers.length ; j++ ) {
+                        numbers[j] = numbers[ j+1 ];                                  
+                    };
+                    for ( let j = i; j < symbols.length ; j++ ) {
+                        symbols[j] = symbols[ j+1 ];
+                    };    
+                numbers.length -= 1;
+                symbols.length -= 1;             
+            }           
         };
-        arr = fArr;
-    }
-    return arr[0];
+    }; 
+    return numbers[0];
 }
 
 module.exports = {
