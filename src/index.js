a5d48e0245e409a997a6fa34489f8c8ba69b70d6
Denis Esis <presetslr@gmail.com>
2019-10-09 17:52:22 +0300

    feat: add start structure

--- src/index.js
+++ src/index.js
@@ -4,7 +4,32 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    console.log(expr);
+
+    const operators = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
+        '(': ')'
+    };
+
+        let stack = [];
+
+        expr.split(' ').join('').split('').forEach((el) => {
+            console.log(el);
+
+            if (el in operators) {
+
+                stack.push(operators);
+            } else {
+                stack.push(el);
+            }
+            console.log(stack);
+        });
+
+        return stack.pop();
+
 }
 
 module.exports = {
