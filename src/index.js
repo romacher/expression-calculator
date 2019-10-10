faf70982146a5372b3e5d51c2a94eed20bb8e21f
Denis <presetslr@gmail.com>
2019-10-10 03:53:08 +0300

    fix: fix logic

--- src/index.js
+++ src/index.js
@@ -4,31 +4,76 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    console.log(expr);
-
+    expr = expr
+        .replace("+", " + ")
+        .replace("-", " - ")
+        .replace("*", " * ")
+        .replace("/", " / ")
+        .replace("(", " ( ")
+        .replace(")", " ) ");
     const operators = {
         '+': (a, b) => a + b,
         '-': (a, b) => a - b,
         '*': (a, b) => a * b,
         '/': (a, b) => a / b,
-        '(': ')'
     };
 
-        let stack = [];
+    const priority = {
+        '+': 2,
+        '-': 2,
+        '*': 3,
+        '/': 3,
+        '(': 0,
+        ')': 1,
+    };
+    let outLine = [];
+    let stack = [];
+    const reg = /\s+/g;
 
-        expr.split(' ').join('').split('').forEach((el) => {
-            console.log(el);
-
-            if (el in operators) {
-
-                stack.push(operators);
-            } else {
+    console.log(expr.trim().split(reg));
+    expr.trim().split(reg).forEach((el) => {
+        // console.log(el);
+        if (el in priority) {
+            // console.log('+++ ' + el);
+            if (stack.length === 0)
                 stack.push(el);
+            else if (el === ')') {
+                for (let i = stack.length - 1; i >= 0; i--) {
+                    if (stack[i] !== '(') {
+                        outLine.push(stack.pop());
+                    } else {
+                        stack.splice(-1, 1);
+                        break;
+                    }
+                }
+            } else {
+                for (let i = stack.length - 1; i >= 0; i--) {
+                    if (priority[stack[stack.length - 1]] >= priority[el]) {
+                        outLine.push(stack.pop());
+                    } else {
+                        stack.push(el);
+                        break;
+                    }
+                }
             }
-            console.log(stack);
-        });
+            /* stack.length === 0 ? stack.push(el)
+                 : (priority[stack[stack.length - 1]] === 1)
+                 ? stack.reverse()[stack.length - 1]
+                 forEach(el => el !== '(' // если последний элемент в стэке
+                     ? outLine.push(stack.reverse().pop()) : stack.reverse().slice(0, -1))
+                 : (priority[stack[stack.length - 1]] >= priority[el]) ? stack.reverse().forEach(el => priority[el] >= priority[this.el]
+                     ? outLine.push(stack.reverse().pop()) : stack.push(el)):stack.reverse();*/
+        } else {
+            outLine.push(el);
+        }
+    });
+    if (stack.length !== 0)
+        outLine.push(stack.pop());
 
-        return stack.pop();
+    console.log(stack);
+    console.log(outLine);
+
+    return console.log(outLine.pop());
 
 }
 
